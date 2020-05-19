b2c789eb8761d834c7ee2ee76f16ab3a4c08d041
abhya944 <64408298+abhya944@users.noreply.github.com>
2020-05-19 22:47:51 +0530

    Create program1.py

---
+++ day1/program1.py
@@ -0,0 +1,11 @@
+#general detail of student
+a=input("enter your name:")
+b=input("branch name:")
+c=input("gender:")
+d=input("college name")
+e=int(input("age:"))
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
