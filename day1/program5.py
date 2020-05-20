94c153f020aa14f8495b346e4cfa94c8045f4376
abhya944 <64408298+abhya944@users.noreply.github.com>
2020-05-20 12:04:34 +0530

    Create program5.py

---
+++ day1/program5.py
@@ -0,0 +1,16 @@
+#cricket score card
+a=int(input("1 player run:"))
+b=int(input("2 player run:"))
+c=int(input("3 player run:"))
+d= a/60
+e= b/60
+f= c/60
+print("strike rate frst player:",d)
+print("strike rate seocnd player:",e)
+print("strike rate third player:",f)
+g=print(" new run first player:", a+(60*d))
+h=print(" new run second player:", b+(60*e))
+i=print(" new run third player:", c+(60*f))
+j=print("no of six by first player:",a/6)
+k=print("no of six by second player:",b/6)
+l=print("no of six by third player:",c/6)
