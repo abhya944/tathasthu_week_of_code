5b9f74a559cacb35922791a88b068fb3b6946559
Abhya singh <64408298+abhya944@users.noreply.github.com>
2020-05-20 15:30:47 +0530

    Update program4.py

--- day1/program4.py
+++ day1/program4.py
@@ -3,7 +3,8 @@ a=int(input("cost price:"))
 b=int(input("selling price:"))
 profit=b-a
 profitpercent=(profit*100)/a
-print("profit from this sell:",profitpercent)
+print("profit :",profit)
+print("profit percent from this sell:",profitpercent)
 newprofit=profitpercent+5
 PROFIT=newprofit*a/100
 sp=PROFIT+a
